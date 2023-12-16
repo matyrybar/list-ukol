ca7cb3ecf61bc0780b3b75b513f063efad2a3c62
matej <matej.rybar@educanet.cz>
2023-12-16 22:45:55 +0100

    ukol

---
+++ script.js
@@ -0,0 +1,52 @@
+
+let comments = [
+    {
+        name: "Vito Kraus",
+        date: " 12. 16.2023. 10:23",
+        text: "toto je opravdu skvele..😉"
+    },
+    {
+        name: "Filip Másilko",
+        date: "12. 9. 2023. 19:21",
+        text: "strasne se mi to libi😍"
+    },
+    {
+        name: "Filip Másilko",
+        date: "12. 9. 2023. 19:21",
+        text: "strasne se mi to libi😍"
+    },
+    {
+        name: "Filip Másilko",
+        date: "12. 9. 2023. 19:21",
+        text: "strasne se mi to libi😍"
+    },
+    {
+        name: "Filip Másilko",
+        date: "12. 9. 2023. 19:21",
+        text: "strasne se mi to libi😍"
+    },
+];
+
+let commentsAll = document.getElementById("comments");
+
+comments.forEach(comment => {
+    let commenty = document.createElement("div");
+    commenty.classList.add("comment");
+
+    let autor = document.createElement("div");
+    autor.classList.add("commentautor");
+    autor.textContent = comment.name;
+
+    let date = document.createElement("div");
+    date.classList.add("date");
+    date.textContent = comment.date;
+
+    let text = document.createElement("p");
+    text.textContent = comment.text;
+
+    commenty.appendChild(autor);
+    commenty.appendChild(date);
+    commenty.appendChild(text);
+
+    commentsAll.appendChild(commenty);
+});
